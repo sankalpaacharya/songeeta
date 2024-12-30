42b20dbb0f8eb7e470b724a78857b5a723b1610f
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-12-30 23:17:39 +0530

    comment: add comment for the basic workflow

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,19 @@ class Member extends User{
     }
     
 }
+ 
 
-const sankalpa = new Member("sankalpa","sankalpa@gmail.com",new Date(2024-11-22),"nepal",["Lofi"])
-console.log(sankalpa)
+/*
+----basic WORKFLOW for us----
+I'm gonna make a login,signup, auth middleware class and will work accordingly 
+first signup, login, check the auth middleware(rate limiting maybe?)
+select the genres of music
+listen music if one is play don't allow other to play
+and when user is listening music keep collecting the information and later we can use
+
+*/ 
+const sankalpa = new Member("sankalpa","sankalpa@gmail.com",new Date(2024-11-22),"nepal",["Rap"])
+
 
 
 
