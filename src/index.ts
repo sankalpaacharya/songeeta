58e7e39ab2993b5fde20929422ffa46ec345e8a8
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-12-30 23:12:01 +0530

    add: tsx for watching files

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,24 @@ class User{
         this.dob = dob
         this.country = country
     }
+
 }
 
 class Member extends User{
-    constructor(protected name:string,protected email:string, protected dob:Date,protected country:string, genre:Genre[]){
+    constructor(protected name:string,protected email:string, protected dob:Date,protected country:string, protected genres:Genre[]){
         super(name,email,dob,country)
+        this.genres = genres
     }
+
+    displayInformation(member:Member){
+        console.log(`Name: ${member.name} \nEmail:${member.email}`)
+    }
+    
 }
+
+const sankalpa = new Member("sankalpa","sankalpa@gmail.com",new Date(2024-11-22),"nepal",["Lofi"])
+console.log(sankalpa)
+
 
 
 
